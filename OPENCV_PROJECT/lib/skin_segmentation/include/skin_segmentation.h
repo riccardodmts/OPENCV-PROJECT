62e203af3b1d411bc7124966e3c0526916abb4d3
mattiasecchiero <81315250+mattiasecchiero@users.noreply.github.com>
2022-06-27 15:50:36 +0200

    Update skin_segmentation.h

--- OPENCV_PROJECT/lib/skin_segmentation/include/skin_segmentation.h
+++ OPENCV_PROJECT/lib/skin_segmentation/include/skin_segmentation.h
@@ -9,9 +9,9 @@
 #include <opencv2/imgproc.hpp>
 #include <opencv2/highgui.hpp>
 
+#endif
+
 
 cv::Mat K_Means(cv::Mat Input, int K, cv::Mat &RGB_centers);
 
 cv::Mat get_skin(cv::Mat Input_image);
-
-#endif
