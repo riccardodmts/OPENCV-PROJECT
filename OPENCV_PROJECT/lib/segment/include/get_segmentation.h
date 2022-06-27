69885d5bfc7a79fed00c74a794a2619a8f6ad065
mattiasecchiero <81315250+mattiasecchiero@users.noreply.github.com>
2022-06-27 15:52:19 +0200

    Add files via upload

---
+++ OPENCV_PROJECT/lib/segment/include/get_segmentation.h
@@ -0,0 +1,11 @@
+//get_segmentation.h
+
+#ifndef SEGMENT_h
+#define SEGMENT_h
+
+#include <opencv2/opencv.hpp>
+
+#endif
+
+
+void get_segmentation(float sigma, float k, int min_size, const char *input_path, const char *output_path);
