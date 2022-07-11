c363f70a38779e7a05cb2d0d98f094765be9ab54
riccardodmts <83876494+riccardodmts@users.noreply.github.com>
2022-07-11 18:56:35 +0200

    Update detection.h

--- OPENCV_PROJECT/lib/detection/include/detection.h
+++ OPENCV_PROJECT/lib/detection/include/detection.h
@@ -42,11 +42,11 @@ class HandDetector{
         //change params and specify path for .cfg and .weights
         HandDetector(float conf_th, float nms_th, int size, const std::string& cfg_path, const std::string& weights_path);
         
-        //this method return the bounding boxes and the corresponding confidence levels
+        //this method returns the bounding boxes and the corresponding confidence levels
         //it calls HandDetector::process_results
         void detect_hands(const cv::Mat& image, std::vector<float>& confs, std::vector<cv::Rect>& bboxes);
 
-        //this method detect hands and draws the bounding boxes on output (output will be an hard copy of image)
+        //this method detects hands and draws the bounding boxes on output (output will be an hard copy of image)
         //to draw, it calls HandDetector::draw_bboxes , while for the detection it calls the previous HandDetector::detect_hands
         void detect_hands(const cv::Mat& image, cv::Mat& output);
 
