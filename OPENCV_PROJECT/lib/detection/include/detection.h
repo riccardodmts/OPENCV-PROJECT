49e383e8ece7185dabfbb80daf7f8b4540964bc3
riccardodmts <83876494+riccardodmts@users.noreply.github.com>
2022-07-13 18:14:07 +0200

    Update detection.h

--- OPENCV_PROJECT/lib/detection/include/detection.h
+++ OPENCV_PROJECT/lib/detection/include/detection.h
@@ -1,8 +1,5 @@
 //detection.h
 //author: Riccardo De Monte
-//hours spent: 2
-
-
 
 #ifndef _STD_
 #define _STD_
@@ -42,11 +39,11 @@ class HandDetector{
         //change params and specify path for .cfg and .weights
         HandDetector(float conf_th, float nms_th, int size, const std::string& cfg_path, const std::string& weights_path);
         
-        //this method returns the bounding boxes and the corresponding confidence levels
+        //this method return the bounding boxes and the corresponding confidence levels
         //it calls HandDetector::process_results
         void detect_hands(const cv::Mat& image, std::vector<float>& confs, std::vector<cv::Rect>& bboxes);
 
-        //this method detects hands and draws the bounding boxes on output (output will be an hard copy of image)
+        //this method detect hands and draws the bounding boxes on output (output will be an hard copy of image)
         //to draw, it calls HandDetector::draw_bboxes , while for the detection it calls the previous HandDetector::detect_hands
         void detect_hands(const cv::Mat& image, cv::Mat& output);
 
@@ -57,5 +54,6 @@ class HandDetector{
 
         //this method processes the output of the CNN
         void process_results(const std::vector<cv::Mat>& output, std::vector<float> confs, std::vector<cv::Rect>& bboxes, int rows, int cols);
-
+        void resize_bbox(cv::Rect& bbox, int rows, int cols);
+        
 };
