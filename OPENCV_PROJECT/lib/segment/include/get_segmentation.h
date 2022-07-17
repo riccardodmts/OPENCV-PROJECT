e4a2112ea45ec86f6231c13291d23524ba74bd7b
riccardodmts <83876494+riccardodmts@users.noreply.github.com>
2022-07-17 18:29:09 +0200

    Update get_segmentation.h

--- OPENCV_PROJECT/lib/segment/include/get_segmentation.h
+++ OPENCV_PROJECT/lib/segment/include/get_segmentation.h
@@ -43,15 +43,35 @@ class HandSegmentor{
     //PRIVATE METHODS
 
     private:
-
+        //this function creates a vector of masks: a mask for each segmented region
         void get_masks_per_region(cv::Mat& seg_roi, std::vector<cv::Mat>& masks);
+    
+        //this function returns a new rect in order to expand a roi (it verifies the limits of the image)
         void get_expanded_roi(const cv::Mat& img, const cv::Rect& roi, cv::Rect& new_roi, int max_disp);
+    
+        //to get the mask with the right dimensions since an expansion is applied before
         void get_mask_original_size(const cv::Rect& roi, const cv::Rect& new_roi, const cv::Mat& mask, cv::Mat& new_mask);
+        
+        //it calls the method above for each mask in a vector
         void get_mask_original_size(const cv::Rect& roi, const cv::Rect& new_roi, const std::vector<cv::Mat>& masks, std::vector<cv::Mat>& new_masks);
+    
+        //test if a regions contains or not an hand (0.5 and 0.6 tests are applied)
         int test_region(const std::vector<cv::Rect>& boxes, const cv::Rect& original_box);
+        
+        //it computes the indexes of those regions that satisfies the test (oterating over different masks, it calls test_region for each mask)
         void get_idx_of_regions(const cv::Mat& roi_img, const std::vector<cv::Mat>& masks, std::vector<int>& idxs, const cv::Rect& original_box);
+    
+        //it computes the union of many masks
         void get_mask_union(const std::vector<cv::Mat>& masks, std::vector<int>& idxs, cv::Mat& final_mask);
+        
+        //it computes the intersection between two masks
         void intersect_masks(const cv::Mat& input_union, const cv::Mat& input_skin, cv::Mat& output);
+    
+        //it returns the mask associated to the biggest segmented region
+        void get_biggest_region(cv::Mat& seg_roi, cv::Mat& mask);
+    
+        //function used by get_biggest_region
+        int count_nozero_pixels(const cv::Mat& mask);
 
 
 
