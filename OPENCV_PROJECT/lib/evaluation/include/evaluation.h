59d01333b2041742a192509ec67663240f84b644
Simone Cecchinato <59653823+simonececchinato@users.noreply.github.com>
2022-07-18 23:16:30 +0200

    Update evaluation.h

--- OPENCV_PROJECT/lib/evaluation/include/evaluation.h
+++ OPENCV_PROJECT/lib/evaluation/include/evaluation.h
@@ -14,19 +14,17 @@ class Evaluation{
 
   private:
 
-    cv::Mat image;
-    cv::Mat true_mask;
-    cv::Mat detected_mask;
-    std::vector<cv::Rect> true_bbox;
-    std::vector<cv::Rect> detected_bbox;
-    bool truth;
+    cv::Mat image; //source image
+    cv::Mat true_mask; //image with the true mask
+    cv::Mat detected_mask; //image with the detected mask
+    std::vector<cv::Rect> true_bbox; //vector with all the true bbox
+    std::vector<cv::Rect> detected_bbox; //vector with all the detected bbox
+    bool truth; //auxiliary variable to know if the ground-truth is provided
 
     /*
     function that assign to the true bboxes, the corresponding detected bbox
 
     assign means, to consider the detected bbox that maximize the IoU for that true bbox
-    if the detected bbox are less than the true bbox, we copy from the
-    detected bbox vector, the bbox that maximize the IoU for the remaining true bboxes.
     */
     void ValidateBoundingBox();
 
@@ -39,14 +37,59 @@ class Evaluation{
 
     // return the IoU value of the two bounding boxes
     float IoU(cv::Rect bbox_A, cv::Rect bbox_B);
-    cv::Mat Accuracy();
+
+    //method that perform the accuracy for the mode without the ground-truth
+    //(called in the PixelAccuracy method when there is not the ground-truth)
+    std::vector<cv::Mat> Accuracy();
 
   public:
+
+    //constuctor that uses all the path
     Evaluation(std::string img_path, std::string true_mask_path, cv::Mat our_mask, std::string bbox_path, std::vector<cv::Rect> our_bbox);
+
+    /*
+      from the path of the test image, the true mask path and the real bbox path
+      are obtained. Then the first constructor is recalled
+
+      in this case I suppose to have the following folder hierarchy
+
+      test
+        |-> rgb (with the test images)
+        |-> mask (with all the true mask)
+        |-> det (with all the true bbox)
+
+      */
     Evaluation(std::string img_path, cv::Mat our_mask, std::vector<cv::Rect> our_bbox);
+
+    /*
+      constructor for the mode without the ground-truth
+    */
     Evaluation(std::string img_path, cv::Mat our_mask, std::vector<cv::Rect> our_bbox, int aux);
 
+    /*
+      calculate all the IoU value for the real bounding boxes
+      accept a parameter which is the mode
+      - mode = 0 -> the method return the image with the detected bbox,
+                    the IoU results are printed on console
+      - mode = 1 -> the method return an image with all the detected bbox, the
+                    real bbox and the IoU results for each true bbox
+
+      if the method is used without iniatilizing the ground-truth on the constructor,
+      the output is always equal to the mode 0 (there are not the IoU results)
+    */
     cv::Mat IoU(int mode);
-    cv::Mat PixelAccuracy();
+
+    /*
+      Return a vector of two images: the first one is the image with the segmented hands,
+      the second image is the confusion matrix image.
+      On console are printed the result of accuracy, precision, recall,
+      balance accuracy and F1 score
+
+      if the method is used without iniatilizing the ground-truth on the constructor,
+      the output has still the segmented image as first image,
+      the second image of the vector will be the mask of the detected hands
+    */
+    std::vector<cv::Mat> PixelAccuracy();
 
 };
+
