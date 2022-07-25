073a4a1dc9bb5b19737142944db17a4b5c4b9a0b
mattiasecchiero <81315250+mattiasecchiero@users.noreply.github.com>
2022-07-25 10:24:01 +0200

    Update skin_segmentation.h

--- OPENCV_PROJECT/lib/skin_segmentation/include/skin_segmentation.h
+++ OPENCV_PROJECT/lib/skin_segmentation/include/skin_segmentation.h
@@ -8,8 +8,10 @@
 
 #endif
 
-//implementation of the k-means clustering algorithm
+//implementation of the k-means clustering algorithm: given an image, the number of clusters (k) and the centroids initial values it will 
+//segment the image based on k-means algorithm and returns a cv::Mat output image
 cv::Mat K_Means(cv::Mat Input, int K, cv::Mat &RGB_centers);
 
-//compute the region of the image that can be considered as skin
+//compute the region of the image that can be considered as skin. Given an input image it checks the intensity values of each pixel to 
+// understand if it can be considered as skin or not, based on some thresholds
 cv::Mat get_skin(cv::Mat Input_image);
